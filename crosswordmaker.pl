e20a52ad9503a9098e5ec1354e2ef790bc9d16eb
Kacka Nova <kateteckanew@gmail.com>
2020-09-26 10:52:23 +0200

    verze s dvěma směry

--- crosswordmaker.pl
+++ crosswordmaker.pl
@@ -3,15 +3,14 @@
 %%%%% Kateřina Nová
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%mainMaker(+InputFile,+OutputFile,+MaxWidth,+Secret)
-mainMaker(InputFile,OutputFile,Width,Secret):-
+%mainMaker(+InputFile,+OutputFile,+Width,+Height,+Secret)
+mainMaker(InputFile,OutputFile,Width,Height,Secret):-
     readWords(InputFile,WordsList),
     string_chars(Secret,SecretChars),
-    length(SecretChars,Height),
     makeCrossword(WordsList,SecretChars,Width,Height,SquaresList,KeyList),
     drawTable(SquaresList,Height,Table),
     reverse(Table,Output),
-    listToFile(OutputFile,Output).
+    listToFile(OutputFile,Output,KeyList).
 
 %%%%% Načtení vstupu %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %readWords(+File,-WordsList)
@@ -28,24 +27,21 @@ rowsToWordsList([row(Key,Answer)|RowT],[(Key,Chars)|WordsListT]):-
 %%%%%% Tvorba křížovky %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %makeCrossword(+WordsList,+SecretChars, +Width, +Height, -SquareList, -KeyList)
 makeCrossword(WordsList,SecretChars,Width,Height,SquaresList,KeysList):-
-    listNtoM(1,Width,List),
-    member(Y1,List),
-    makeCrossword1([("tajenka",SecretChars)||WordsList],Width,Height,WordSquares,SquaresList,KeysList),
-    complete(KeysList,Width,Height),!.
+    placeWord(SecretChars,Width,Height,WordSquares),
+    addKey(WordSquares,tajenka,KeysListR,KeysList),
+    makeCrossword1(WordsList,Width,Height,WordSquares,SquaresList,KeysListR),
+    complete(SquaresList,Width,Height),!.
 
 %makeCrossword1(+WordsList, +Width, +Height, +Acumulator, -SquaresList, -KeyList)
 makeCrossword1([],_,_,A,A,[]).
 makeCrossword1([(Key,CharsList)|WordT],Width,Height,A,SquaresList,KeysList):-
     placeWord(CharsList,Width, Height, WordSquares),
-    legalWord(WordSquares,A),!,
+    legalWord(WordSquares,A),
     addKey(WordSquares,Key,KeysListR,KeysList),
     append(WordSquares,A,SquaresListR),
     makeCrossword1(WordT,Width,Height,SquaresListR,SquaresList,KeysListR).
 
 %placeWord(+CharsList, +Width, +Height, -WordSquares)
-placeWord(CharsList,Width,_,[]):-
-    length(CharsList, Len),
-    Width - Len < 0,!.
 placeWord(ListOfChars,Width,Height,WordSquares):-
     length(ListOfChars, Len),
     listNtoM(1,Width-Len+1,YList),
@@ -55,8 +51,8 @@ placeWord(ListOfChars,Width,Height,WordSquares):-
     placeWord1(ListOfChars, (X,Y),horizontal,WordSquares).
 placeWord(ListOfChars,Width,Height,WordSquares):-
     length(ListOfChars, Len),
-    listNtoM(1,Width-Len+1,YList),
-    listNtoM(1,Height,XList),
+    listNtoM(1,Width,YList),
+    listNtoM(1,Height-Len+1,XList),
     member(X,XList),
     member(Y,YList),
     placeWord1(ListOfChars, (X,Y),vertical,WordSquares).
@@ -84,23 +80,26 @@ listNtoM(N,M,[N|List]):-
 
 %legalWord(+WordSquares,+SquaresList)
 legalWord([],_).
-legalWord(WordSquares,SquareList):-
-    member((X1,Y1,L,horizontal),WordSquares),
-    member((X1,Y1,L,vertical),SquareList),!.
-legalWord(WordSquares,SquareList):-
-    member((X1,Y1,L,vertical),WordSquares),
-    member((X1,Y1,L,horizontal),SquareList),!.
-legalWord(WordSquares,SquareList):-
-    member((X1,Y1,_,_),WordSquares),
-    \+member((X1,Y1,_,_),SquareList),!.
+legalWord([(X1,Y1,L,Direction)|WordSquares],SquareList):-
+    oposite(Direction,ODirection),
+    member((X1,Y1,L,ODirection),SquareList),
+    legalWord(WordSquares,SquareList).
 
-%addKey(+SquaresList, +OldKeysList, -NewKeysList)  
+legalWord([(X1,Y1,_,_)|WordSquares],SquareList):-
+    \+member((X1,Y1,_,_),SquareList),
+    legalWord(WordSquares,SquareList).
+
+%oposite(+Direction,-Direction).
+oposite(vertical,horizontal).
+oposite(horizontal,vertical).
+
+%addKey(+SquaresList, +Key, +OldKeysList, -NewKeysList)  
 addKey([],_,KeyList,KeyList).
-addKey([(X,_,_,_)|_],Key,KeyList,[(X,Key)|KeyList]).
+addKey([(X,Y,_,Direction)|_],Key,KeyList,[(X,Y,Direction,Key)|KeyList]).
 
 %complete(+KeysList, +Height)
 complete(SquareList,Width,Height):-
-    Size is Width * Height,
+    Size is Width * Height *2,
     length(SquareList,Len),
     Size == Len.
 
@@ -109,9 +108,9 @@ complete(SquareList,Width,Height):-
 %drawTable(+SquaresList,+Height,-Table)
 drawTable([],0,[]):-!.
 drawTable(SquareList,X,[Row|Output]):-
-    findall((Y,L), member((X,Y,L,vertical),SquareList), RowBag),
-    findall((Y,L), \+member((X,_,L,_),SquareList),SquareListR),
-    sort(1,@>,RowBag,Sorted),
+    bagof((Y,L), member((X,Y,L,vertical),SquareList), RowBag),
+    delete(SquareList,(X,_,L,_),SquareListR),
+    sort(1,@<,RowBag,Sorted),
     drawRow(Sorted,Row),
     NX is X -1,
     drawTable(SquareListR,NX,Output).
@@ -119,19 +118,20 @@ drawTable(SquareList,X,[Row|Output]):-
 
 %drawRow(+SquaresList, -RowList)
 drawRow([],[]).
-drawRow([[(_,L)|List],[L|Row]):-drawRow(List,Row).
+drawRow([(_,L)|List],[L|Row]):-drawRow(List,Row).
 
 %writeList(+File, +List)
 writeList(_File, []) :- !.
-writeList(File, [(Key,LetterList)|T]) :-
+writeList(File, [LetterList|T]) :-
     write(File,LetterList),
-    write(File, '\t'),
-    write(File, Key),
+    %write(File, '\t'),
+    %write(File, Key),
     write(File, '\n'),
     writeList(File, T).
 
 %listToFile(+Filename, +List)
-listToFile(Filename,List) :-
+listToFile(Filename,List1,List2) :-
+    append(List1,List2,List),
     open(Filename, write, File),
     writeList(File, List),
     close(File).
--- dokumentace.md
+++ dokumentace.md
@@ -1,25 +1,29 @@
 # Crosswordmaker
 
 ### Popis programu
-Program dostane seznam dvojic klíč-odpověď, tajenku a maximální šířku tabulky. Z odpovědí sestaví tabulku tak, aby v jednom sloupci svisle vycházela zadaná tajenka. V každém řádku výsledné tabulky se nachází právě jedna položka. Ty mohou začínat i končit různě, ale všechny musí zasahovat do tajenkového sloupce.
+Program dostane seznam dvojic klíč-odpověď, tajenku a velikost tabulky. Odpovědi a tajenku umístí do tabulky tak, aby každé políčko bylo součástí právě dvou slov (horizontálně a vertikálně). Pak vypíše seznam klíčů s odpovídajícími souřadnicemi a směrem jejích odpovědí.
 
 ### Implementace
 #### Načtení vstupu
 Program dostane jméno souboru, ve kterém se nachází dvojice klíč-odpověd. Každá dvojice se musí nacházet na samostatném řádku a klíč a odpověď jsou odděleny čárkou. Jak klíč, tak odpověď mohou obsahovat víc slov. Procedura *readWords* a *rowsToWordsList* načtou obsah souboru a vytvoří seznam dvojic (Klíč, Seznam znaků odpovědi).
-Také tajenka se převede na seznam znaků a z jeho délky se zjistí výška tabulky.
+Také tajenka se převede na seznam znaků.
 
 #### Tvorba křížovky
 Jednotlivá políčka tabulky jsou reprezentovány čtveřicí (Xová souřadnice, Yová souřadnice, Znak, Směr). 
-Procedura *makeCrossword* nederministicky umístí tajenku do nějakého sloupce a pak zavolá *makeCrossword1*, která umístí do tabulky zbylá slova. 
-Procedura *makeCrossword1* rekurzivně prochází seznam slov a každé se pomocí procedury *placeWord* pokusí umístit do tabulky. Procedura *legalWord* kontroluje, jestli takto umístěné slovo splňuje požadované vlastnosti (protíná sloupec tajenky a v daném řádku už není jiné slovo). Pokud ano, přidá jeho klíč do seznamu klíčů pomocí procedury *addKey* a jeho políčka do seznamu všech políček.
-Procedura *placeWord* funguje nedeterministicky - dané slovo se buď vůbec nepoužije - v tom případě se vrátí prázdný seznam a nebo pomocí *member* deterministicky umístí slovo na všechny možné pozice a vrátí seznam odpovídajícíh políček.
+Procedura *makeCrossword* nederministicky umístí tajenku a pak zavolá *makeCrossword1*, která umístí do tabulky zbylá slova. 
+Procedura *makeCrossword1* rekurzivně prochází seznam slov a každé se pomocí procedury *placeWord* pokusí umístit do tabulky. Procedura *legalWord* kontroluje, jestli takto umístěné slovo splňuje požadované vlastnosti (pro každé políčko platí, že je tam stejné písmeno, ale v opačném směru, nebo se tam zatím nic nevyskytuje). Pokud ano, přidá jeho klíč do seznamu klíčů pomocí procedury *addKey* a jeho políčka do seznamu všech políček.
+Procedura *placeWord* funguje nedeterministicky, dané slovo se buď vůbec nepoužije - v tom případě se vrátí prázdný seznam, umístí se horizontálně nebo vertikálně - pak pomocí *member* deterministicky umístí slovo na všechny možné pozice a vrátí seznam odpovídajícíh políček.
 Umisťování do tabulky na konkrétní místo a v konkrétním směru má na starosti rekurzivní procedura *placeWord1*. 
-Po vytvoření tajenky se pomocí procedura *complete* zkontroluje, zda je počet klíčů shodný s výškou tabulky. Protože předtím kontrolujeme, zda v jednom řádku není víc klíčů, je tento term splněný pouze pokud každý řádek má právě jeden klíč.
+Po vytvoření tajenky se pomocí procedura *complete* zkontroluje, zda se počet vytvořených políček rovná dvojnásobku velikosti tabulky. Díky *legalWord* se nemůže stát, že by ve vytvořených políčkách bylo nějaké víckrát než dvakrát.
 
 #### Tisk výsledků
-Nejprve procedury *drawTable* a *drawRow* převedou list jednotlivých políček na tabulku. Ta se pomocí *listToFile* zapíše do souboru. Výstup tvoří vyplněná tajenka s klíčem na každém řádku.
+Nejprve procedury *drawTable* a *drawRow* převedou list jednotlivých políček na tabulku. Ta se pomocí *listToFile* zapíše do souboru spolu se seznamem klíčů. Výstup tvoří vyplněná tajenka a seznam klíčů s odpovídajícími souřadnicemi.
 
 ### Testovací data
-Vstupní testovací data jsou uložena v souboru *test.csv*. Následující příkaz vytvoří z testovacích dat křížovku s tajenkou PROLOG a maximální šířkou deset políček a zapíše ji do souboru *krizovka.txt*.
+Vstupní testovací data jsou uložena v souboru *test.csv* (delší slova s klíči) a *test2.csv* (tří písmená slova bez klíčů). 
 
-mainMaker("test.csv","krizovka.txt",10,"prolog").
+Fungující příkaz:
+mainMaker("test2.csv","krizovka2.txt",3,3,cat).
+
+Následující příkaz nedoběhne:
+mainMaker("test.csv","krizovka.txt",6,6,prolog).
